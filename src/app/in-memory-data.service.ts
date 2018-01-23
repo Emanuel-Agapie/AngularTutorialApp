bb81287b3746e6e017972058c0133a5a7bf1d9ca
Emanuel Agapie <eagapie@codeless.com>
2018-01-23 18:34:39 +0200

    updated the app to be able to add/delete/update/search heroes

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,20 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 1, name: 'Golf V R32'},
+      { id: 2, name: 'Golf V GTI' },
+      { id: 3, name: 'Golf VII GTI Clubsport Ed.40' },
+      { id: 4, name: 'Mustang V6 2012' },
+      { id: 5, name: 'Mustang GT 2012' },
+      { id: 6, name: 'Mustang GT BOSS 302' },
+      { id: 7, name: 'Mustang GT 550' },
+      { id: 8, name: 'Mustang GT 350' },
+      { id: 9, name: 'Mustang BULLIT' },
+      { id: 10, name: 'Dodge Demon SRT' },
+      { id: 11, name: 'Audi S4 B 8.5' }
+    ];
+    return {heroes};
+  }
+}
